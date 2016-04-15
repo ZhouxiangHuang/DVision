2f068c70fdf27a39e01c92c67eef52bd07806a5a
apeltz <aaron.peltz@gmail.com>
2016-04-15 16:04:14 -0700

    re-added watchify; fixed server routing for dvision localhost

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ var path = require('path');
 var open = require('open');
 var dvisionserver = express();
 
-// dvisionserver.use(express.static(__dirname + '/index.html'));
 
-dvisionserver.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + req.url))
+dvisionserver.get(/.html$|.css$|.js$/, function(req, res){
+  //file paths will need to change to navigate out of user npm_modules directory
+  if(req.url==='/index.html'){
+    res.sendFile(path.join(__dirname + '/index.html'));
+  } else{
+    res.sendFile(path.join(__dirname + req.url));
+  }
 });
 
+//new port established for hosting and rendering database tables
 var port = 10001;
 dvisionserver.listen(port, function() {
   console.log('dvision server listening on port ' + port);
-  open('http://localhost:'+port);
+  open('http://localhost:'+port+'/index.html');
 })
